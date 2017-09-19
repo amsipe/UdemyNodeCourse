1afb089c07a30c6043f55513201fd3369c98d0db
Anderson Sipe <amsipe@gmail.com>
2017-09-19 18:53:32 -0400

    adding projects page

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.get('/bad',(req,res) => {
         error: 'couldnt do it'
     });
 })
+
+app.get('/projects',(req,res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page',
+
+    });
+})
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
